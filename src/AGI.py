e5410e6e669443f19efb6787a1f73811eee11c82
mferrari <mferrari@ethz.ch>
2024-02-20 14:38:44 +0100

    Encapsulated main driver into class

--- src/AGI.py
+++ src/AGI.py
@@ -3,89 +3,96 @@ import os
 import argparse
 
 # Driver functions for the profiler module
-def profile(args):
-    from AGI.utils.checkImports import checkDCGMImports
-    from AGI.io.MetricsDataIO import MetricsDataIO
-    
-    # Check if DCGM bindings are available before importing AGI modules
-    # This raises an exception if DCGM is not available
-    checkDCGMImports()
-    # Import AGI modules
-    from AGI.profiler.GPUMetricsProfiler import GPUMetricsProfiler
-    
-    # Check if job was called via SLURM
-    # We need this in order to extract information regarding which GPUs to monitor from the environment
-    try:
-        jobId = os.environ['SLURM_JOB_ID']
-        # Assume exactly one GPU per rank for now
-        # It is important to set --gpus-per-task=1 in the SLURM script for this to work
-        gpuIds = [int(gpu) for gpu in os.environ['SLURM_STEP_GPUS'].strip().split(',')]
-    except KeyError:
-        # Hack - if --gpus-per-task=1 is not set, we can still use SLURM_PROCID mod 4 to determine the GPU ID
-        print("WARNING: SLURM environment variables not found. Using SLURM_PROCID mod 4 to determine GPU ID.")
-        jobId = os.environ['SLURM_JOB_ID']
-        procId = int(os.environ['SLURM_PROCID'])
-        gpuIds = [procId%4]
-
-    # Set default output file if not specified
-    if args.output_file is None:
-        args.output_file = f"AGI_{jobId}.sqlite"
+class AGI:
+    def __init__(self, args):
+        self.args = args
+    def profile(self):
+        from AGI.utils.utils import checkDCGMImports
+        from AGI.io.MetricsDataIO import MetricsDataIO
         
-    # Create profiler object
-    profiler = GPUMetricsProfiler(
-        gpuIds=gpuIds, # Need to pass a list of GPU IDs
-        samplingTime=args.sampling_time,
-        maxRuntime=args.max_runtime,
-    )
+        # Check if DCGM bindings are available before importing AGI modules
+        # This raises an exception if DCGM is not available
+        checkDCGMImports()
+        # Import AGI modules
+        from AGI.profiler.GPUMetricsProfiler import GPUMetricsProfiler
+        
+        # Check if job was called via SLURM
+        # We need this in order to extract information regarding which GPUs to monitor from the environment
+        try:
+            jobId = os.environ['SLURM_JOB_ID']
+            # Assume exactly one GPU per rank for now
+            # It is important to set --gpus-per-task=1 in the SLURM script for this to work
+            gpuIds = [int(gpu) for gpu in os.environ['SLURM_STEP_GPUS'].strip().split(',')]
+        except KeyError:
+            # Hack - if --gpus-per-task=1 is not set, we can still use SLURM_PROCID mod 4 to determine the GPU ID
+            print("WARNING: SLURM environment variables not found. Using SLURM_PROCID mod 4 to determine GPU ID.")
+            jobId = os.environ['SLURM_JOB_ID']
+            procId = int(os.environ['SLURM_PROCID'])
+            gpuIds = [procId%4]
 
-    # Run workload
-    if args.wrap:
-        profiler.run(args.wrap)
-    else:
-        # Open args.script
-        with open(args.script) as f:
-            cmds = f.read().split('\n')
+        # Set default output file if not specified
+        if self.args.output_file is None:
+            self.args.output_file = f"AGI_{jobId}.sqlite"
+            
+        # Create profiler object
+        profiler = GPUMetricsProfiler(
+            gpuIds=gpuIds, # Need to pass a list of GPU IDs
+            samplingTime=self.args.sampling_time,
+            maxRuntime=self.args.max_runtime,
+        )
 
-        for cmd in cmds:
-            if cmd and not cmd.startswith('#'):
-                profiler.run(cmd)
+        # Run workload
+        if self.args.wrap:
+            profiler.run(self.args.wrap)
+        else:
+            # Open self.args.script
+            with open(self.args.script) as f:
+                cmds = f.read().split('\n')
 
-    # Get collected metrics
-    metrics = profiler.getCollectedData()
+            for cmd in cmds:
+                if cmd and not cmd.startswith('#'):
+                    profiler.run(cmd)
 
-    # Create IO handler
-    IO = MetricsDataIO(args.output_file, readOnly=False, forceOverwrite=args.force_overwrite)
+        # Get collected metrics
+        metrics = profiler.getCollectedData()
 
-    # Dump data to SQL DB
-    IO.dump(metrics)
-    
-    return 0
+        # Create IO handler
+        IO = MetricsDataIO(self.args.output_file, readOnly=False, forceOverwrite=self.args.force_overwrite)
 
-# Driver function for the analyze module
-def analyze(args):
-    from AGI.analysis.analysis import GPUMetricsAnalyzer
-    
-    # Instantiate analyzer class
-    analyzer = GPUMetricsAnalyzer(inputFile=args.input_file)
+        # Dump data to SQL DB
+        IO.dump(metrics)
+        
+        return 0
 
-    # If necessary, remove outliers
-    if args.detect_outliers != 'none':
-        # This modifies the analyzer object in-place
-        analyzer.detectOutlierSamples(args.detect_outliers)
+    # Driver function for the analyze module
+    def analyze(self):
+        from AGI.analysis.analysis import GPUMetricsAnalyzer
+        
+        # Instantiate analyzer class
+        analyzer = GPUMetricsAnalyzer(inputFile=self.args.input_file)
 
-    # Print summary of metrics
-    if args.summary:
-        analyzer.summary(args.verbose)
-    
-    # Plot time-series of metrics
-    if args.plot_time_series:
-        analyzer.plotTimeSeries()
+        # If necessary, remove outliers
+        if self.args.detect_outliers != 'none':
+            # This modifies the analyzer object in-place
+            analyzer.detectOutlierSamples(self.args.detect_outliers)
 
-    # Plot load-balancing of metrics
-    if args.plot_load_balancing:
-        analyzer.plotUsageMap()
-    
-    return 0
+        # Print GPU information
+        if self.args.show_gpus:
+            analyzer.showGPUs()
+
+        # Print summary of metrics
+        if self.args.summary:
+            analyzer.summary(self.args.verbose)
+        
+        # Plot time-series of metrics
+        if self.args.plot_time_series:
+            analyzer.plotTimeSeries()
+
+        # Plot load-balancing of metrics
+        if self.args.plot_load_balancing:
+            analyzer.plotUsageMap()
+        
+        return 0
 
 if __name__ == '__main__':
     # Main parser
@@ -111,6 +118,7 @@ if __name__ == '__main__':
     parser_analyze = subparsers.add_parser('analyze', help='Analyze command help')
     parser_analyze.add_argument('--input-file', '-i', type=str, required=True, help='Input file for analysis')
     parser_analyze.add_argument('--summary', '-s', type=bool, help='Print summary of metrics. Default is True.')
+    parser_analyze.add_argument('--show-gpus', '-g', action='store_true', help='Print GPU information')
     parser_analyze.add_argument('--verbose', '-v', action='store_true', help='Print verbose GPU metrics to stdout')
     parser_analyze.add_argument('--detect-outliers', '-d', type=str, default='leading', choices=['leading', 'trailing', 'none', 'all'],
                                 help='Heuristically detect outlier samples and discard them from the analysis')
@@ -121,8 +129,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Run appropriate command
+    AGIObj = AGI(args)
     if args.subcommand == 'profile':
-        profile(args)  # or a specific function for profiling
+        AGIObj.profile()  # or a specific function for profiling
     elif args.subcommand == 'analyze':
-        analyze(args)  # or a specific function for analysis
+        AGIObj.analyze()  # or a specific function for analysis
 
