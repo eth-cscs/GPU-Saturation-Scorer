2f45a2c303e3f5208dd75b5057cbff4586e90f9a
Henrique <henrique.mendonca@cscs.ch>
2025-04-05 16:10:16 +0200

    make it installable

--- src/AGI/AGI.py
+++ src/AGI/AGI.py
@@ -247,3 +247,46 @@ class AGI:
         if self.args.report:
             analyzer.report()
 
+
+def main():
+    """
+    Main function to run the AGI tool.
+    It sets up the command line argument parser, imports necessary modules,
+    and runs the appropriate subcommand based on the parsed arguments.
+    """
+    # Main parser
+    parser = argparse.ArgumentParser(description='Monitor and analyze resource usage of a workload with AGI')
+
+    # Subparsers
+    subparsers = parser.add_subparsers(dest='subcommand', help='sub-command help')
+
+    # Profile subcommand
+    parser_profile = subparsers.add_parser('profile', help='Profile command help')
+    parser_profile.add_argument('--wrap', '-w', metavar='wrap', type=str, nargs='+', help='Wrapped command to run', required=True)
+    parser_profile.add_argument('--label', '-l', metavar='label', type=str, help='Workload label', required=True)
+    parser_profile.add_argument('--max-runtime', '-m', metavar='max-runtime', type=int, default=0, help='Maximum runtime of the wrapped command in seconds')
+    parser_profile.add_argument('--sampling-time', '-t', metavar='sampling-time', type=int, default=500, help='Sampling time of GPU metrics in milliseconds')
+    parser_profile.add_argument('--force-overwrite', '-f', action='store_true', help='Force overwrite of output file', default=False)
+    parser_profile.add_argument('--append', '-a', action='store_true', help='Append profiling data to the output file', default=False)
+    parser_profile.add_argument('--output-folder', '-o', metavar='output-folder', type=str, default=None, help='Output folder for the profiling data', required=True)
+
+    # Analyze subcommand
+    parser_analyze = subparsers.add_parser('analyze', help='Analyze command help')
+    parser_analyze.add_argument('--input', '-i', type=str, required=True, help='Input folder or SQL file for analysis')
+    parser_analyze.add_argument('--silent', '-s', action="store_true", default=False, help='Silent mode')
+    parser_analyze.add_argument('--report', '-rp', action="store_true", default=False, help='Generate full PDF report')
+    parser_analyze.add_argument('--export', '-e', metavar='export', type=str, default=":memory:", help='SQLite database file to export the raw data (default: in-memory database)')
+    parser_analyze.add_argument('--output', '-o', type=str, required=False, help='Output file for analysis')
+    parser_analyze.add_argument('--force-overwrite', '-f', action='store_true', help='Force overwrite of output file', default=False)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Run appropriate command
+    agi_obj = AGI(args)
+
+    if args.subcommand in ['profile', 'export', 'analyze']:
+        agi_obj.run()
+    else:
+        # Print help if no valid subcommand is given
+        parser.print_help()
